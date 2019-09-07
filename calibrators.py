create notebook complexity scaling, fix bug in pav-calibrator

calibrators.py
@@ -101,7 +101,9 @@ class PAVCalibrator(Calibrator):
 
         # We use 1 vs rest approach, perform a
         # isotonic regression for each class
-        self.models = [IsotonicRegression(y_min=0, y_max=1)
+        self.models = [IsotonicRegression(y_min=0,
+                                          y_max=1,
+                                          out_of_bounds='clip')
                        for _ in range(self.n_classes)]
 
         self.fit(self.logits, self.target)
@@ -109,17 +111,14 @@ class PAVCalibrator(Calibrator):
     def fit(self, logits, target):
 
         probs = softmax(logits, axis=1)
-        for cls in range(self.n_classes):
-            model = self.models[cls]
-
+        for cls, model in enumerate(self.models):
             x, y = probs[:, cls], target[:, cls]
             model.fit(x, y)
 
     def predict(self, logits):
         probs = softmax(logits, axis=1)
         cal_probs = np.zeros(logits.shape)
-        for cls in range(self.n_classes):
-            model = self.models[cls]
+        for cls, model in enumerate(self.models):
             cal_probs[:, cls] = model.predict(probs[:, cls])
 
         cal_probs /= np.sum(cal_probs, axis=1, keepdims=True)

utils/visualization.py
@@ -10,7 +10,7 @@ from .ops import project_sequence, project_point, onehot_encode
 from .metrics import empirical_cross_entropy
 
 # Regions ListedColormap
-sequ = np.linspace(0, 1, 256)
+sequ = np.linspace(0.33, 1, 256)
 new_colors = np.zeros((256*3, 3))
 for i in range(3):
     new_colors[256*i:256*(i+1), i] = sequ
@@ -170,6 +170,13 @@ def plot_cal_regions_ternary(calibrator,
             style="hexagonal",
             cmap=REGS_CMAP,
             colorbar=False)
+
+    # Create legend
+    tax.legend(handles=[
+            mpatches.Patch(color='red', label=labels[0]),
+            mpatches.Patch(color='green', label=labels[1]),
+            mpatches.Patch(color='blue', label=labels[2])
+            ])
 
     tax.boundary(linewidth=2.0)
     tax.set_title(title+'\n\n', fontsize=fontsize+2)